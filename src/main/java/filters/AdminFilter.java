24dcad4b3adc805d9fb8be7134b563465b2c6fc7
IgorBastos <IgorBastos@IgorPC>
2015-06-26 14:39:53 -0300

    Corrigindo o Filter para direcionar ao admin.jsp sempre que não estiver logado

--- src/main/java/filters/AdminFilter.java
+++ src/main/java/filters/AdminFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import servlet.ServletUtil;
 
-@WebFilter(urlPatterns = "*.ko")
+@WebFilter(urlPatterns = { "*.ko", "/ko-admin/*" })
 public class AdminFilter implements Filter {
 
 	public void destroy() {
